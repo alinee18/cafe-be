2e9b1c840addd8a0390f6787c22049279f42297d
alinee18 <marsyacarissa_atmono_33rpl@student.smktelkom-mlg.sch.id>
2026-06-01 21:04:18 +0700

    Fix: paksa update sinkronisasi controller dan service order

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -31,7 +31,6 @@ export class OrderController {
     @Req() req: any, 
     @Body() dto: { items: { menuId: number; qty: number }[]; reservationId?: number } // 🔥 Tambahkan reservationId? di sini
   ) {
-    // 🔥 Alirkan parameter dto.reservationId ke dalam service sebagai argumen ketiga
     return this.orderService.createOrder(req.user.id, dto.items, dto.reservationId);
   }
 
